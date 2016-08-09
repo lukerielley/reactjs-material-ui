1ea6276bfd3912830965ee5ee345b7634c4a37bf
Luke Rielley <luke.rielley@gmail.com>
2016-08-09 20:57:20 +1000

    Migrating to step wizard

--- src/app/Main.js
+++ src/app/Main.js
@@ -18,10 +18,13 @@ import MenuItem from 'material-ui/MenuItem';
 import LinearProgress from 'material-ui/LinearProgress';
 import Slider from 'material-ui/Slider';
 
+import TextField from 'material-ui/TextField';
+import HeaderComponent from './Components/header.component.js';
+import FooterComponent from './Components/footer.component.js';
+
 const styles = {
   container: {
-    textAlign: 'center',
-    paddingTop: 20,
+    textAlign: 'center'
   },
 };
 
@@ -35,12 +38,10 @@ const paper_style = {
   height: 100,
   width: 100,
   margin: 20,
-  textAlign: 'center',
   display: 'inline-block',
 };
 
 class Main extends Component {
-
 
   constructor(props, context) {
     super(props, context);
@@ -91,95 +92,72 @@ class Main extends Component {
 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
+
         <div style={styles.container}>
 
-        <img src="/images/SmallLogo.png" width="200" />
+        <HeaderComponent></HeaderComponent>
 
-        <Slider 
-          style={{
-              width: '300px',
-              margin: '0 auto'
-            }}
-          min={3}
-          max={10}
-          step={1}
-          defaultValue={5}
-          value={this.state.loanTerm}
-          onChange={this.updateLoanTerm.bind(this)}
-        />
-        <Slider 
-          style={{
-              width: '300px',
-              margin: '0 auto'
-            }}
-          min={3000}
-          max={55000}
-          step={100}
-          defaultValue={5000}
-          value={this.state.loanAmount}
-          onChange={this.updateLoanAmount.bind(this)}
-        />
+        <div className="pageContent">
 
-        <p>
-          <h3>{this.state.loanTerm} Years</h3>
-          <h3>${this.state.loanAmount}</h3>
-        </p>
+              <h1>How much can I borrow?</h1>
+              <p>Find out your estimated personal loan repayments as well as ways to pay off your personal loan faster</p>
+              
+              <LoanWizardVerticalStepper></LoanWizardVerticalStepper>
 
-        <h1>Loan Repayment Calculator</h1>
-        <p>Find out your estimated personal loan repayments as well as ways to pay off your personal loan faster</p>
-
-          <Dialog
-            open={this.state.open}
-            title="Dialog Title"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            <p>This is the dialog content</p>
-          </Dialog>
+              <h1>Loan Repayment Calculator</h1>
+              <p>Estimate how much you can borrow and how much the repayments will be at the current interest rate.</p>
+              
+              
+              <Dialog
+                open={this.state.open}
+                title="Dialog Title"
+                actions={standardActions}
+                onRequestClose={this.handleRequestClose}
+              >
+                <p>This is the dialog content</p>
+              </Dialog>
 
 
-          <Checkbox
-            id="checkboxId1"
-            name="checkboxName1"
-            value="checkboxValue1"
-            label="I accept the terms and conditions"
-            style={{
-              width: '300px',
-              margin: '0 auto'
-            }}
-            iconStyle={{
-              fill: '#FF0000'
-            }}/>
+              <Checkbox
+                id="checkboxId1"
+                name="checkboxName1"
+                value="checkboxValue1"
+                label="I accept the terms and conditions"
+                style={{
+                  width: '300px',
+                  margin: '0 auto'
+                }}
+                iconStyle={{
+                  fill: '#FF0000'
+                }}/>
 
-          <div>
-            <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-              <MenuItem value={1} primaryText="4.75% - Variable Rate" />
-              <MenuItem value={2} primaryText="3.99% - Fixed Rate 1 year" />
-              <MenuItem value={3} primaryText="3.75% - Fixed Rate 2 years" />
-              <MenuItem value={4} primaryText="3.89% - Fixed Rate 3 years" />
-              <MenuItem value={5} primaryText="3.99% - Fixed Rate 4 years" />
-            </DropDownMenu>
+              <div>
+                
+              </div>
+
+              <Paper style={paper_style} zDepth={4} circle={true} >
+                <CircularProgress
+                style={{
+                  padding: '15px'
+                }}
+                />
+              </Paper>
+
+              <br />
+              <LinearProgress mode="indeterminate" />
+              <br />
+
+              
+              <RaisedButton
+                label="Show Dialog"
+                secondary={true}
+                onTouchTap={this.handleTouchTap}
+              />
+
           </div>
 
-          <LoanWizardVerticalStepper></LoanWizardVerticalStepper>
+          <FooterComponent></FooterComponent>
 
-          <Paper style={paper_style} zDepth={4} circle={true} >
-            <CircularProgress
-             style={{
-              padding: '15px'
-            }}
-             />
-          </Paper>
-
-          <br />
-          <LinearProgress mode="indeterminate" />
-          <br />
-
-          <RaisedButton
-            label="Show Dialog"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
         </div>
       </MuiThemeProvider>
     );
